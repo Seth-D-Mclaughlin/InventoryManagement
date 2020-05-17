7ff4505b47c33528eb8a4a76a1b7c6c817f4bf44
Seth McLaughlin <Seth.d.mclaughlin@gmail.com>
2020-05-17 10:47:36 -0400

    Add project files.

---
+++ Inventory.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Inventory.MVC.Startup))]
+namespace Inventory.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
