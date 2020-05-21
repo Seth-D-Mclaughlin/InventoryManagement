834e2b6bf74ec9fa5a33805cb6278795df9aa6ad
Seth McLaughlin <seth.d.mclaughlin@gmail.com>
2020-05-20 23:18:28 -0400

    Added Models MemoryDetail, MemoryEdit and MemList

---
+++ Inventory.MODELS/MemeoryModels/MemoryListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.MODELS.MemeoryModels
+{
+    public class MemoryListItem
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        public string Speed { get; set; }
+
+        [Required]
+        [Display(Name = "In use?")]
+        public bool IsAvailable { get; set; }
+    }
+}
